145347017ed66d80fdd08ca8796f9a655b4a15d0
Taehee Kim <rabolution@gmail.com>
2022-11-12 21:15:40 +0900

    chore: netlify adaper

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    // Enable Preact to support Preact JSX components.
-    preact(),
-  ],
-  site: `http://astro.build`
+  integrations: [tailwind(),
+  // Enable Preact to support Preact JSX components.
+  preact()],
+  site: `http://astro.build`,
+  output: "server",
+  adapter: netlify()
 });
